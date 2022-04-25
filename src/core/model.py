0bfe9a0b2ff3c9aada472071a131b2fa27dccd0c
GitBolt <saabisakhtar@gmail.com>
2022-04-25 14:35:47 +0530

    improve: strucutre

---
+++ src/core/model.py
@@ -0,0 +1,34 @@
+
+import torch
+import torch.nn as nn
+import string
+import random
+import sys
+import unidecode
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.embed = nn.Embedding(input_size, hidden_size)
+        self.lstm = nn.LSTM(hidden_size, hidden_size,
+                            num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x, hidden, cell):
+        out = self.embed(x)
+        out, (hidden, cell) = self.lstm(out.unsqueeze(1), (hidden, cell))
+        out = self.fc(out.reshape(out.shape[0], -1))
+        return out, (hidden, cell)
+
+    def init_hidden(self, batch_size):
+        hidden = torch.zeros(self.num_layers, batch_size,
+                             self.hidden_size).to(device)
+        cell = torch.zeros(self.num_layers, batch_size,
+                           self.hidden_size).to(device)
+        return hidden, cell
