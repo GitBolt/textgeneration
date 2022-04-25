c05d1eaed5dd6a7274bcc784f4ab222282ac1e14
GitBolt <saabisakhtar@gmail.com>
2022-04-25 14:36:22 +0530

    improve: strucutre

--- src/main.py
+++ src/main.py
@@ -2,24 +2,41 @@ import string
 import torch
 from fastapi import FastAPI
 from pydantic import BaseModel
-from eval import generate
+from core.evaluate import generate
+from core.model import RNN
 
 all_characters = string.printable
 n_characters = len(all_characters)
-
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-app = FastAPI()
+model = RNN(
+    n_characters,
+    256,
+    2,
+    n_characters
+).to(device)
+model.load_state_dict(torch.load(
+    "data/data.pt",
+    map_location=torch.device(device))
+)
+model.eval()
 
+app = FastAPI()
 
 class Content(BaseModel):
     content: str
     length: int
 
+
 @app.get("/")
 async def index():
     return {"message": "hi, go to /docs"}
-    
+
+
 @app.post("/kashbot")
 async def kashbot(content: Content):
-    return generate(content.dict()["content"], content.dict()["length"])
+    return generate(
+        model,
+        content.dict()["content"],
+        content.dict()["length"]
+    )
